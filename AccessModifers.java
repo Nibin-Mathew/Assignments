e8ff7e7ca46ab0580826a11cf17c34ff2a91a42d
NibinKMathew <sajinisajan89@gmail.com>
2020-10-19 19:37:10 -0700

    Addintional files Access Modifiers,Main program

---
+++ AccessModifers.java
@@ -0,0 +1,29 @@
+package assignmentsSimpliLearn;
+
+public class AccessModifers {
+	
+	public void Variables(){
+		int x=50;
+		System.out.println("This is public " +x);
+		
+	}
+private void Variables1(){
+	int y=30;
+	System.out.println("This is private " +y);
+	
+}
+
+ protected void Variables3(){
+	 double z=10.19;
+	 System.out.println("This is protected " +z);
+	 
+	 
+	
+}
+ 
+ void Variables4(){
+	 int w=10;
+	 System.out.println("This is default " +w);
+	
+ }
+}
---
+++ MainProgram.java
@@ -0,0 +1,15 @@
+package assignmentsSimpliLearn;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		
+		AccessModifers am=new AccessModifers();
+		
+		am.Variables();
+		am.Variables3();
+		am.Variables4();
+
+	}
+
+}
