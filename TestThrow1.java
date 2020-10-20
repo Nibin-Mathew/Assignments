c03501ba40f7e24e6bb107291da523128877c605
NibinKMathew <sajinisajan89@gmail.com>
2020-10-20 12:51:28 -0700

    Throws file addition

---
+++ TestThrow1.java
@@ -0,0 +1,19 @@
+package assignmentsSimpliLearn;
+
+public class TestThrow1 {
+	
+	public  void validate(int age){
+		
+		if(age<18){
+			throw new ArithmeticException("not valid)");
+		}
+			else{
+				System.out.println("Welcome to Vote");
+		
+		}
+			
+			
+		}
+	}
+
+
