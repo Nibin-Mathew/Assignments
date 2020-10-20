dd00c64b2a3a651a6a94554b3f77a0f55d777b09
NibinKMathew <sajinisajan89@gmail.com>
2020-10-19 19:50:34 -0700

    additional files-While loop

---
+++ WhileLoop.java
@@ -0,0 +1,17 @@
+package assignmentsSimpliLearn;
+
+public class WhileLoop {
+	
+	public static void main(String[] args) {
+		int i=1;
+		int len=20;
+		while (i<=len/2){
+		
+			System.out.println(i);
+			i++;
+		}
+
+	}
+	}
+	
+	
